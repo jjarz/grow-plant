6fc279948677a5798dfa9416f295b0701aa58781
Julie Jarzemsky <jjarzem@gmail.com>
2017-08-05 09:53:14 -0500

    Clean up: Revert "Instructional comments (will revert)"
    This reverts commit a7429286706e865b643144e160b3cacc11512a38.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-// load es6 modules
-// more here: https://leanpub.com/understandinges6/read#leanpub-auto-encapsulating-code-with-modules
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// es6 classes
-// more here: https://strongloop.com/strongblog/an-introduction-to-javascript-es6-classes/
-
-// Component:
-// - let you split the UI into independent, reusable pieces,
-// and think about each piece in isolation.
-// - Can be pure JavaScript or JSX
 class App extends Component {
-
-  // every React Component is required to have a render method
   render() {
     return (
-      // JSX syntax
-      // React forms a Javascript representation of the actual DOM using this (referred to as the "Virtual DOM")
-      // Virtual DOM allows React to compare current Virtual DOM with the previous one, and only updated
-      // the parts of the DOM that have changed- https://facebook.github.io/react/img/docs/granular-dom-updates.gif)
-      // JSX will be converted to React.createElement https://facebook.github.io/react/docs/react-api.html#createelement
-      //  React.createElement(
-      //    type,
-      //    [props],
-      //    [...children]
-      //  )
-      // See more here: https://facebook.github.io/react/docs/jsx-in-depth.html
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -40,5 +18,4 @@ class App extends Component {
   }
 }
 
-// es6 export
 export default App;
