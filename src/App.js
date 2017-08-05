6a446e79f1604cd1c71498b545646fed2092bb8f
Julie Jarzemsky <jjarzem@gmail.com>
2017-08-05 11:48:11 -0500

    Add points controls to grow plant

--- src/App.js
+++ src/App.js
@@ -5,21 +5,48 @@ import UserFeedback from './components/UserFeedback'
 import stateUtils from './stateUtils'
 import './App.css';
 
+const POINTS = {
+  SEEDLING: 5,
+  SMALL: 20,
+  FULL_GROWN: 50
+}
+
 class App extends Component {
   constructor(props) {
     super(props) // must call this since we are overriding Component's constructor
 
     // initial state
     this.state = {
-      plantSize: 0
+      plantSize: 0,
+      points: 0,
+      feedbackText: stateUtils.feedback.start
     }
   }
 
-  // arrow function allow this function to use this w/o explicitly binding it
-  handleWaterClick = () => {
-    this.setState((prevState, props) => ({
-      plantSize: prevState.plantSize + 1
-    }))
+  handlePlantGrowth = (points) => {
+    const newPoints = this.state.points + points
+
+    this.setState(() => {
+      return { points: newPoints }
+    })
+
+    if (newPoints >= POINTS.FULL_GROWN) {
+      this.setState(() => ({
+        plantSize: 3
+      }))
+    } else if (newPoints >= POINTS.SMALL) {
+      this.setState(() => {
+        return {
+          plantSize: 2
+        }
+      })
+    } else if (newPoints >= POINTS.SEEDLING) {
+      this.setState(() => {
+        return {
+          plantSize: 1
+        }
+      })
+    }
   }
 
   render() {
@@ -33,11 +60,12 @@ class App extends Component {
           <Plant plantSize={stateUtils.plantSizes[this.state.plantSize]}/>
 
           {/* control buttons */}
-          <ControlButton control={stateUtils.controls.water} onClick={this.handleWaterClick}/>
-          <ControlButton control={stateUtils.controls.light} />
-          <ControlButton control={stateUtils.controls.love} />
+          <ControlButton control={stateUtils.controls.water} points={2} onClick={this.handlePlantGrowth} />
+          <ControlButton control={stateUtils.controls.light} points={4} onClick={this.handlePlantGrowth} />
+          <ControlButton control={stateUtils.controls.love} points={6} onClick={this.handlePlantGrowth} />
 
           <UserFeedback feedbackText={stateUtils.feedback.start} />
+          {this.state.points}
           <div>
 
           </div>
--- src/components/ControlButton.js
+++ src/components/ControlButton.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 
 class ControlButton extends Component {
+  // arrow function allow this function to use this w/o explicitly binding it
+  handleClick = () => {
+    this.props.onClick(this.props.points)
+  }
+
   render () {
     return (
-      <button onClick={this.props.onClick}>{this.props.control}</button>
+      <button onClick={this.handleClick}>{this.props.control}</button>
     )
   }
 }
