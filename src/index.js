a7429286706e865b643144e160b3cacc11512a38
Julie Jarzemsky <jjarzem@gmail.com>
2017-08-05 09:52:32 -0500

    Instructional comments (will revert)

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// render App componenet to the DOM element with id: 'root'
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
